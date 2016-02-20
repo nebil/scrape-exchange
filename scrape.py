a4a337608d5d67d483e241cd9b2c6126262bbceb
Nebil Kawas <nkawas90@gmail.com>
2016-02-20 16:46:40 -0300

    :frog: Adapted the code to harvest data from multiple sites
    This commit also includes a text file with eight popular sites.

--- scrape.py
+++ scrape.py
@@ -9,16 +9,28 @@ You can obtain a copy of the MPL at <https://www.mozilla.org/MPL/2.0/>.
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'http://stackoverflow.com/users?tab=NewUsers&sort=creationdate'
+URL_PREFIX = 'http://'
+URL_SUFFIX = '/users?tab=NewUsers&sort=creationdate'
+FILENAME = 'sites.txt'
 
 
-def get_last_user_id():
-    rget = requests.get(URL)
-    soup = BeautifulSoup(rget.content, 'html.parser')
+def scrape():
+    def get_last_user_id(curl):
+        rget = requests.get(URL_PREFIX + curl + URL_SUFFIX)
+        soup = BeautifulSoup(rget.content, 'html.parser')
 
-    # search for the most recent user.
-    user = soup.find('div', class_='user-details').find('a')['href']
-    user_id = user.split('/')[2]
-    print(user_id)
+        # search for the most recent user.
+        user = soup.find('div', class_='user-details').find('a')['href']
+        user_id = user.split('/')[2]
+        return user_id
 
-get_last_user_id()
+    with open(FILENAME, 'r') as sites:
+        for site in sites:
+            name, curl = site.split('|')
+            curl = curl.strip()
+            if '.com' not in curl:
+                curl += '.stackexchange.com'
+
+            print(name, get_last_user_id(curl))
+
+scrape()

9b8e6fb633c2276e5ce0a3fa7e447758912fe153
Nebil Kawas <nkawas90@gmail.com>
2016-02-21 21:20:19 -0300

    :lipstick: Refined the output format by sorting and aligning

--- scrape.py
+++ scrape.py
@@ -16,21 +16,27 @@ FILENAME = 'sites.txt'
 
 def scrape():
     def get_last_user_id(curl):
+        if '.com' not in curl:
+            curl += '.stackexchange.com'
+
         rget = requests.get(URL_PREFIX + curl + URL_SUFFIX)
         soup = BeautifulSoup(rget.content, 'html.parser')
 
         # search for the most recent user.
         user = soup.find('div', class_='user-details').find('a')['href']
         user_id = user.split('/')[2]
-        return user_id
+        return int(user_id)
+
+    def process(line):
+        name, curl = line.split('|')
+        curl = curl.strip()
+
+        return [name, curl, get_last_user_id(curl)]
 
     with open(FILENAME, 'r') as sites:
-        for site in sites:
-            name, curl = site.split('|')
-            curl = curl.strip()
-            if '.com' not in curl:
-                curl += '.stackexchange.com'
-
-            print(name, get_last_user_id(curl))
+        all_sites = [process(site) for site in sites]
+        all_sites.sort(key=lambda site: site[2], reverse=True)
+    for site in all_sites:
+        print('{} | {:18} | {:9,}'.format(*site))
 
 scrape()
